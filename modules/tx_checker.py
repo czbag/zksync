616dd61c70f4b7e12770e5f7a295f59cf4c43be4
czbag <129870465+czbag@users.noreply.github.com>
2023-09-17 04:59:50 +0300

    fix update
    fix tx_checker module
    fix Velocore contract

--- modules/tx_checker.py
+++ modules/tx_checker.py
@@ -1,5 +1,7 @@
+import asyncio
 import random
 
+from eth_typing import ChecksumAddress
 from loguru import logger
 from web3 import Web3, AsyncHTTPProvider
 from web3.eth import AsyncEth
@@ -9,20 +11,32 @@ from tabulate import tabulate
 from config import ACCOUNTS, RPC
 
 
+async def get_nonce(address: ChecksumAddress):
+    web3 = Web3(
+        AsyncHTTPProvider(random.choice(RPC["zksync"]["rpc"])),
+        modules={"eth": (AsyncEth,)},
+        middlewares=[],
+    )
+
+    nonce = await web3.eth.get_transaction_count(address)
+
+    return nonce
+
+
 async def check_tx():
-    data = {}
+    tasks = []
 
     logger.info("Start transaction checker")
-    for pk in ACCOUNTS:
-        web3 = Web3(
-            AsyncHTTPProvider(random.choice(RPC["zksync"]["rpc"])),
-            modules={"eth": (AsyncEth,)},
-            middlewares=[],
-        )
-        account = EthereumAccount.from_key(pk)
-        nonce = await web3.eth.get_transaction_count(account.address)
-        data.update({f"{account.address}": nonce})
 
-    table = [[k + 1, i, data[i]] for k, i in enumerate(data)]
+    for _id, pk in enumerate(ACCOUNTS, start=1):
+        account = EthereumAccount.from_key(pk)
+
+        tasks.append(asyncio.create_task(get_nonce(account.address), name=account.address))
+
+    await asyncio.gather(*tasks)
+
+    table = [[k, i.get_name(), i.result()] for k, i in enumerate(tasks, start=1)]
+
     headers = ["#", "Address", "Nonce"]
+
     print(tabulate(table, headers, tablefmt="github"))
